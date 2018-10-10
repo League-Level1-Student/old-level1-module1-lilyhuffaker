2c5dd0fa37bb6edf2989b1c1fc29af6c67dba72d
Huffaker.Lily <Huffaker.Lily@SEMS-259-18.SEMS259.LAB>
2018-10-10 16:27:32 -0700

    TACO

--- src/Taco.java
+++ src/Taco.java
@@ -1,4 +1,23 @@
 
 public class Taco {
-
+	private String meat;
+	private String sauce;
+	
+	public String getMeat() {
+		return meat;
+	}
+	
+	public String getSauce() {
+		return sauce;
+	}
+	
+	public void setMeat(String meat) {
+		this.meat = meat;
+	}
+	
+	public void setSauce(String sauce) {
+		this.sauce = sauce;
+	}
+	
+	
 }
